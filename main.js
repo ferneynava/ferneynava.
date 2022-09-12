00509a59b724f656bba4f8399536e0d31300a8cb
ferneynava <ferneynava@gmail.com>
2022-09-11 20:26:48 -0500

    :sparkles: [ADD] nuevas caractísticas y styles
    [ADD] nuevas caractísticas y styles

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const card = document.querySelector(".wrapper__card");
 const imagen_perfil = document.querySelector(".wrapper__image");
-const namee = document.querySelector(".wrapper__name");
 const rect = document.body.getBoundingClientRect();
 const mitadWidth = rect.width/2;
 const mitadHeight =rect.height/2;
@@ -8,17 +7,15 @@ const mitadHeight =rect.height/2;
 window.addEventListener("mousemove", (event) => {
         card.style.transition = "none";
         imagen_perfil.style.transition = "none";
-        namee.style.transition = "none";
 
         const offSetX = event.pageX;
         const offSetY = event.pageY;
 
-         const rotationX = ((offSetX -  mitadWidth) /  mitadWidth) * 15;
-         const rotationY = ((offSetY -  mitadHeight) / mitadWidth) * 15;
+         const rotationX = ((offSetX -  mitadWidth) /  mitadWidth) * 22;
+         const rotationY = ((offSetY -  mitadHeight) / mitadWidth) * 22;
 
         card.style.transform = "rotateX("+ rotationX + "deg) rotateY("+ rotationY + "deg)";
         imagen_perfil.style.transform = "rotateX("+ rotationX + "deg) rotateY("+ rotationY + "deg)";
-        namee.style.transform = "rotateX("+ rotationX + "deg) rotateY("+ rotationY + "deg)";
         
 })
 
@@ -28,7 +25,4 @@ window.addEventListener("mouseout", () => {
         
         imagen_perfil.style.transition = "transform .5s ease-in-out";
         imagen_perfil.style.transform = "rotateX(0deg) rotateY(0deg)";
-
-        namee.style.transition = "transform .5s ease-in-out";
-        namee.style.transform = "rotateX(0deg) rotateY(0deg)";
 })
